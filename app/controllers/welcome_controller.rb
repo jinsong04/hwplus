020b166e16233ed4272d69830a30f5cf93d910cc
Teddy Jinsong Hwang <hwangjinsong@gmail.com>
2017-03-20 13:10:14 +0000

    두번째 수업

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @homeworks = ["Science", "History", "Math", "Design", "Painting", "Singing"]
+    @hwcontent = ["Finish Lab", "Finish Martin Luther", "Finish p.284", "Design this", "Paint that", "Sing this"]
+  end
+
+  def detail
   end
 end
